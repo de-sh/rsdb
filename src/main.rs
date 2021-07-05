ffcce5668f933627e0af413a0fae7aa39e5ae3d0
Devdutt Shenoi <devdutt@outlook.in>
2021-07-05 19:00:56 +0530

    Add initial crate content

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+//! rsdb is an experimental relational database written in rust, inspired by SQLite
+//!
+//! The idea with this experiment is to implement a simple and easy to understand
+//! rewrite SQLite in rust-lang. This project also tries to be a learning experience
+//! for me and is strictly to be in an educational setting only.
+
+fn main() {}
